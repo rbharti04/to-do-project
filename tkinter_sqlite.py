77782ed06824b20642acfb9cc681e6679ee0eeed
riya <semsswim@gmail.com>
2022-02-09 12:54:37 -0600

    Revert "Massive UI improvements, needs polishing"
    This reverts commit 53e8be357d3c77c2daeff0b2d8043255f8f6ec4d.

--- tkinter_sqlite.py
+++ tkinter_sqlite.py
@@ -4,14 +4,18 @@ from os import remove
 from queue import PriorityQueue
 import tkinter
 import tkinter.messagebox
+<<<<<<< HEAD
 from tkinter import * #imports all tkinter functions and modules
 from tkinter import ttk #style widget
 from tkinter.ttk import Treeview 
+=======
+>>>>>>> parent of 53e8be3 (Massive UI improvements, needs polishing)
 import sqlite3
 from typing import List
 
 
 # Setup the database
+<<<<<<< HEAD
 class Database:
     def __init__(self,db):
         self.conn = sqlite3.connect(db)
@@ -87,6 +91,50 @@ def select_task(event):
         priority_entry.insert(END, selected_item[3])
     except IndexError:
         pass
+=======
+conn = sqlite3.connect('tasks.sqlite')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS tasks(
+   task TEXT,
+   due TEXT,
+   priority INT);
+""")
+conn.commit()
+
+win = tkinter.Tk() #creating the main window and storing the window object in 'win'
+#We create the widgets here
+win.title('To-Do List') #setting the title of the window
+win.geometry('500x400') #setting the size of the window
+
+def add_task():
+    task = entry_task.get()
+    if task != "":
+        full_task = (task, "never", 1)
+        cur.execute("INSERT INTO tasks VALUES(?, ?, ?);", full_task)
+        conn.commit()
+        update_gui()
+    else:
+        tkinter.messagebox.showwarning(title="Warning!", message="You must enter a task.")
+
+def delete_task():
+    try:
+        task_pos = listbox_tasks.curselection()[0] #cur-selection NOT curse-selection
+        task = listbox_tasks.get(task_pos)
+        # WARNING: If duplicate tasks exist, this will currently delete them all!
+        cur.execute("DELETE FROM tasks WHERE task=?;",(str(task[0]),))
+        conn.commit()
+        update_gui()
+    except:
+       tkinter.messagebox.showwarning(title="Warning!", message="You must select a task.") 
+       
+def update_gui():
+    listbox_tasks.delete(0,listbox_tasks.size()) # delete all of the current tasks from the gui
+    cur.execute("SELECT * FROM tasks;")
+    all_results = cur.fetchall()
+    for item in all_results:
+        listbox_tasks.insert(tkinter.END, item) # add each item in our python List back to the gui
+        entry_task.delete(0, tkinter.END)
+>>>>>>> parent of 53e8be3 (Massive UI improvements, needs polishing)
 
 def search_task():
     search = search_task.get()
@@ -97,6 +145,7 @@ def search_priority():
     populate_list2(priority)
 
 #Create GUI
+<<<<<<< HEAD
 frame_search = Frame(app)
 frame_search.grid(row=0, column=0) #using .grid allows you to control which part of the grid the edits will apply to
 
@@ -177,5 +226,29 @@ app.title('To-do List')
 app.geometry('700x550')
 
 populate_list()
+=======
+frame_tasks = tkinter.Frame(win)
+frame_tasks.pack()
+
+listbox_tasks = tkinter.Listbox(frame_tasks,height=15,width=50)
+listbox_tasks.pack(side=tkinter.LEFT)
+
+scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
+scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
+scrollbar_tasks.config(command=listbox_tasks.yview)
+
+entry_task=tkinter.Entry(win, width=50)
+entry_task.pack()
+
+button_add_task = tkinter.Button(win, text="Add task", width=50, command=add_task)
+button_add_task.pack()
+
+button_delete_task = tkinter.Button(win, text="Delete task", width=50, command=delete_task)
+button_delete_task.pack()
+
+update_gui()
+>>>>>>> parent of 53e8be3 (Massive UI improvements, needs polishing)
 
 app.mainloop() #start program
